7ff7045f2ce944fe8c40e4ac2a9d22dcaae73b94
Benjamin-Webb <benjamin.webb@asu.edu>
2022-09-17 10:54:45 -0700

    Completed implementation of both methods

--- Problem2.py
+++ Problem2.py
@@ -37,9 +37,6 @@ def GD_inexact(t, alpha, x0, epsilon0):
 	epsilon = 1.0
 	n = np.uint16(1)
 
-	f0 = np.zeros((1, 1))
-	f = np.zeros((1, 1))
-	phi = np.zeros((1, 1))
 	# Begin gradient descent loop
 	while epsilon > epsilon0:
 		# Problem 2 minimize function ||x - x0||^2
@@ -64,14 +61,13 @@ def GD_inexact(t, alpha, x0, epsilon0):
 			break
 		else:
 			epsilon = np.linalg.norm(f - f0)
-			f0 = f
 			x0 = x
 			alpha = 1.0
-			n = n + 1
+			n = n + np.uint16(1)
 
 	# Print complete statement with # of iterations
 	print('Gradient Descent Optimization Complete')
-	print('# of Iterations: ' + str(n))
+	print('# of Iterations: ' + str(n-1))
 	return x
 
 def newton(x0, epsilon0):
@@ -83,22 +79,54 @@ def newton(x0, epsilon0):
 	epsilon = 1.0
 	n = np.uint16(1)
 
-	f0 = np.zeros((1, 1))
-	f = np.zeros((1, 1))
+	# Calculate Hessian once, since it is constant over domain of this function
+	H0 = hessian()
+	H0_1 = np.linalg.inv(H0)
+
 	# Begin optimization loop
 	while epsilon > epsilon0:
 		# Problem 2 optimzation function
 		f0 = 5.0*x0[0]**2 + 10.0*x0[1]**2 + 12.0*x0[0]*x0[1] - 8.0*x0[0] - 14.0*x0[1] + 5.0
 
-		
+		# Calculate gradient at current point
+		g0 = gradx(x0)
+
+		# Calculate next x values
+		x = x0 - H0_1 @ g0
+
+		# Update optimization function value
+		f2 = 5.0*x[0]**2 + 10.0*x[1]**2 + 12.0*x[0]*x[1] - 8.0*x[0] - 14.0*x[1] + 5.0
+		f = f0 - 0.5*g0.T @ H0_1 @ g0
+
+		# Update stoping criteria and determine next step
+		epsilon = np.linalg.norm(f - f0)
+		x0 = x
+		n = n + np.uint16(1)
+		if n >= 10000:
+			break
+
+	# Print complete statement with # of iterations
+	print('Newton Optimization Complete')
+	print('# of Iterations: ' + str(n-1))
+	return x
+
 
 # Main program for problem 2
 if __name__ == '__main__':
 	# HW2 Problem 2
 
-	x = GD_inexact(1.0, 1.0, np.array([[0.0], [0.0]]), 0.000001)
-	x1 = -2.0*x[0] - 3.0*x[1] + 1
+	# Gradient descent w/ inexact line search
+	x_GD = GD_inexact(1.0, 1.0, np.array([[0.0], [0.0]]), 0.000001)
+	x1_GD = -2.0*x_GD[0] - 3.0*x_GD[1] + 1
 
-	print('x1 = ' + str(x1))
-	print('x2 = ' + str(x[0]))
-	print('x3 = ' + str(x[1]))
+	print('x1 = ' + str(x1_GD))
+	print('x2 = ' + str(x_GD[0]))
+	print('x3 = ' + str(x_GD[1]))
+
+	# Newton's method
+	x_N = newton(np.array([[0.0], [0.0]]), 0.000001)
+	x1_N = -2.0*x_N[0] - 3.0*x_N[1] + 1
+
+	print('x1 = ' + str(x1_N))
+	print('x2 = ' + str(x_N[0]))
+	print('x3 = ' + str(x_N[1]))
